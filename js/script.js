11b44487f12aee52c49ea2851f3acca8a4482f2f
MaxTvorogov <maximtvorogov@gmail.com>
2020-05-18 00:34:02 +0300

    first lesson don

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-const name = 'Max';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-alert(`Hello: ${name}!`);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов','');
+const b = prompt('Оцените его','');
+const c = prompt('Один из последних просмотренных фильмов','');
+const d = prompt('Оцените его','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
